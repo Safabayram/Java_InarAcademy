9bce1de7fe9d4000fdab38f2ffa5371dbf87c6de
safa <sfabyram@hotmail.com>
2022-08-22 00:20:55 +0300

    updated

---
+++ src/chapters/Checkpoint2_1/Checkpoint2_1.java
@@ -0,0 +1,9 @@
+public class Checkpoint2_1 {
+	public static void main(String[] args) {
+		double radius;
+		double area;
+		radius = 20;
+		area = radius * radius * 3.14159;
+		System.out.println("the area for the circle of radius " + radius + " is " + area);
+	}
+}
